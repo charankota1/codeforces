ea070fcb0be375237c1f954b8568b47a1ecb396e
charankota1 <lakshmicharankota@gmail.com>
2026-09-16 14:37:29 +0530

    Add Registration System [C] from Codeforces

---
+++ 4/C - Registration System/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    map<string, int> m;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        if(m.count(s)==0){
+            cout<<"OK"<<endl;
+            m[s]=1;
+        }
+        else{
+            cout<<s<<m[s]<<endl;
+            m[s]++;
+        }
+    }
+}
